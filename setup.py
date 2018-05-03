4da8e42a815eec823bf11913d3d6abadef70a58e
Davis Townsend <eelsivad66@gmail.com>
2018-05-03 11:04:19 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='forecast',
+      version='0.1',
+      description='high level forecast library for python',
+      url='http://github.com/DavisTownsend/forecastt',
+      author='Davis Townsend',
+      author_email='dtownsend@ea.com',
+      license='MIT',
+      packages=['forecast'],
+      zip_safe=False)
