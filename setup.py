3e66c2cf489099ed6dcedd9193f9f419dd8f0841
Davis Townsend <eelsivad66@gmail.com>
2018-05-03 11:25:57 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,9 +3,18 @@ from setuptools import setup
 setup(name='forecast',
       version='0.1',
       description='high level forecast library for python',
+      long_description='This package is intended to be a high level wrapper around other time series analysis libaries in python and R. The intention is to replicate the ease of use functionality of the forecast package in R but in python',
       url='http://github.com/DavisTownsend/forecastt',
       author='Davis Townsend',
       author_email='dtownsend@ea.com',
+      classifiers=[
+        'Development Status :: 1 - Alpha',
+        'License :: MIT License',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Time Series Analysis :: Forecasting',
+      ],
+      keywords='time series analysis forecast forecasting predict model parallel',
       license='MIT',
       packages=['forecast'],
       install_requires=['dask','distributed','rpy2','pystan','fbprophet','plotly']
