68b9bdd5cf87f59004c327a98d7526460b9a3d95
Davis Townsend <eelsivad66@gmail.com>
2018-05-14 11:17:17 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,8 @@ import sys
 from os.path import exists
 import setuptools
 
-setuptools.setup(name='magi',
+setuptools.setup(
+      name=sphinx_doc.__name__',
       version=sphinx_doc.__version__,
       description='high level wrapper for parallel univariate time series forecasting',
       long_description=open('README.rst').read() if exists('README.rst') else '',
@@ -19,6 +20,7 @@ setuptools.setup(name='magi',
       ],
       keywords='time series analysis forecast forecasting predict model parallel',
       license='MIT',
+      py_modules=['sphinx_doc'],
       packages=[sphinx_doc.__name__],
       python_requires='~=3.5',
       install_requires=['numpy','pandas','dask','distributed','pystan','rpy2','fbprophet','plotly','cufflinks'],
